0d459d3da76bc2c1f58a05b75c418978b127eb46
jcortessilva <125971033+jcortessilva@users.noreply.github.com>
2024-11-16 21:43:36 +0100

    AP.py

--- AP.py
+++ AP.py
@@ -1,8 +1,8 @@
 import streamlit as st
+import os
 
 # Debug: Confirm Streamlit is running
 st.write("Hello! Streamlit app is running.")
 
-# Debug: Confirm where the app is running
-import os
+# Debug: Print the current working directory
 st.write("Current working directory:", os.getcwd())
