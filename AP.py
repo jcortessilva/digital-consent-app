c10475d45ef820fcdbe74a16e06acfbe6402a9d9
jcortessilva <125971033+jcortessilva@users.noreply.github.com>
2024-11-16 22:02:59 +0100

    AP.py

--- AP.py
+++ AP.py
@@ -1,9 +1,4 @@
 import streamlit as st
-import os
 
-st.experimental_set_query_params(clear_cache=True)  # Forces Streamlit to refresh
-# Debug: Confirm Streamlit is running
-st.write("Hello! Streamlit app is running.")
-
-# Debug: Print the current working directory
-st.write("Current working directory:", os.getcwd())
+# Debug message
+st.write("This is a test message. If you see this, Streamlit is working correctly!")
