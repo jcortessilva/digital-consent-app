0e223ed94a2996979ff86c0ef2f4681d1fcd25b2
jcortessilva <125971033+jcortessilva@users.noreply.github.com>
2024-11-16 21:45:58 +0100

    AP.py

--- AP.py
+++ AP.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 
+st.experimental_set_query_params(clear_cache=True)  # Forces Streamlit to refresh
 # Debug: Confirm Streamlit is running
 st.write("Hello! Streamlit app is running.")
 
